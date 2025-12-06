ac39af79699a3f26db443ed4f168ced3e9d10537
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2025-12-06 13:49:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-TELEGRAM_TOKEN = "YOUR_TOKEN"
-CHAT_IDS = ["123"]
+# config.py
+# Telegram Bot Token'ınız (Sizin token'ınızla güncellendi)
+TELEGRAM_TOKEN = "8588829956:AAEK2-wa75CoHQPjPFEAUU_LElRBduC-_TU"
+
+# Sinyallerin gönderileceği sohbet (chat) ID'lerinin listesi.
+# Sizin ID'nizle güncellendi.
+CHAT_IDS = ["661794787"]
